32f5f9f6cf622d85824f00503650a68ed5e652e3
Temuujinn123 <90952413+Temuujinn123@users.noreply.github.com>
2022-07-14 18:50:20 +0800

    Added login and register pages.

---
+++ pages/account/[account].js
@@ -0,0 +1,140 @@
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Button from "../../Components/button";
+import { Footer } from "../../Components/footer";
+import Header from "../../Components/header";
+
+export default function Login() {
+  const router = useRouter();
+  const account = router.query.account;
+
+  return (
+    <>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <Header className="shadow-sm" noExtraNav />
+      <main className="mt-0 md:mt-[70px]">
+        <div className="py-10 px-4 md:py-[75px] md:px-10">
+          {account === "login" ? (
+            <>
+              <div className="mb-7 md:mb-[50px]">
+                <h1 className="text-[26.35px] md:text-[31px] font-bold leading-[1] text-center">
+                  Login
+                </h1>
+              </div>
+              <div>
+                <label
+                  htmlFor="emailInput"
+                  className="uppercase text-[.75em] md:text-[12px] tracking-[.3em] mb-2 font-medium cursor-pointer block"
+                >
+                  Email
+                </label>
+                <input
+                  id="emailInput"
+                  className="mb-7 py-2 px-3 text-[1rem] w-full border border-[#e8e8e1] block tracking-[.025em] font-medium"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="uppercase text-[.75em] md:text-[12px] tracking-[.3em] mb-2 font-medium cursor-pointer block"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="emailInput"
+                  className="mb-7 py-2 px-3 text-[1rem] w-full border border-[#e8e8e1] block tracking-[.025em] font-medium"
+                />
+              </div>
+              <div className="w-full">
+                <div className="mb-4 w-full">
+                  <Button title="Sign in" className="block md:w-full" />
+                </div>
+                <Link href="/account/register">
+                  <a className="text-[#252d3a] cursor-pointer text-center font-medium text-[13.6px] md:text-[16px] leading-[1.6] tracking-[.025em] block">
+                    Create an account
+                  </a>
+                </Link>
+              </div>
+            </>
+          ) : account === "register" ? (
+            <>
+              <div className="mb-7 md:mb-[50px]">
+                <h1 className="text-[26.35px] md:text-[31px] font-bold leading-[1] text-center">
+                  Create Account
+                </h1>
+              </div>
+              <div>
+                <label
+                  htmlFor="firstNameInput"
+                  className="uppercase text-[.75em] md:text-[12px] tracking-[.3em] mb-2 font-medium cursor-pointer block"
+                >
+                  First name
+                </label>
+                <input
+                  id="firstNameInput"
+                  className="mb-7 py-2 px-3 text-[1rem] w-full border border-[#e8e8e1] block tracking-[.025em] font-medium"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="lastNameInput"
+                  className="uppercase text-[.75em] md:text-[12px] tracking-[.3em] mb-2 font-medium cursor-pointer block"
+                >
+                  Last name
+                </label>
+                <input
+                  id="lastNameInput"
+                  className="mb-7 py-2 px-3 text-[1rem] w-full border border-[#e8e8e1] block tracking-[.025em] font-medium"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="emailInput"
+                  className="uppercase text-[.75em] md:text-[12px] tracking-[.3em] mb-2 font-medium cursor-pointer block"
+                >
+                  Email
+                </label>
+                <input
+                  id="emailInput"
+                  className="mb-7 py-2 px-3 text-[1rem] w-full border border-[#e8e8e1] block tracking-[.025em] font-medium"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="uppercase text-[.75em] md:text-[12px] tracking-[.3em] mb-2 font-medium cursor-pointer block"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  className="mb-7 py-2 px-3 text-[1rem] w-full border border-[#e8e8e1] block tracking-[.025em] font-medium"
+                />
+              </div>
+              <div className="w-full">
+                <div className="mb-4 w-full">
+                  <Button title="Create" className="block md:w-full" />
+                </div>
+                <Link href="/account/login">
+                  <a className="text-[#252d3a] cursor-pointer text-center font-medium text-[13.6px] md:text-[16px] leading-[1.6] tracking-[.025em] block">
+                    Sign in
+                  </a>
+                </Link>
+              </div>
+            </>
+          ) : (
+            <>
+              <div></div>
+            </>
+          )}
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+}
